6be70e06e39407e9f65a8e02349657a29e0e0a4d
hatoo <hato2000@gmail.com>
2021-09-14 22:21:58 +0900

    02_dens_step

--- src/02_dens_step.rs
+++ src/02_dens_step.rs
@@ -1,22 +1,36 @@
-use fluid_animations::{lin_solve, Ghost};
-use ndarray::prelude::*;
+use fluid_animations::{advect, lin_solve, Ghost};
+use glam::Vec2;
+use ndarray::{prelude::*, Zip};
 
 fn main() -> anyhow::Result<()> {
     const N: usize = 400;
     const N_FRAME: usize = 64;
 
     let mut x0 = Array::zeros((N + 2, N + 2));
-    x0[[N / 2, N / 2]] = 500.0;
     let mut x = x0.clone();
 
+    let s = {
+        let mut s = x.clone();
+        s[[N / 2, N / 2]] = 10000.0;
+        s
+    };
+
+    let uv = Array::from_elem(x.dim(), Vec2::new(0.0, -1.0));
+
     let dt = 1.0 / 24.0;
-    let diff = 0.01;
+    let diff = 0.015;
     let a = dt * diff * N as f32 * N as f32;
 
     for f in 1..N_FRAME + 1 {
         fluid_animations::image::save(f, &x0)?;
 
+        Zip::from(&mut x0).and(&s).for_each(|x, &s| {
+            *x += dt * s;
+        });
         lin_solve(&mut x, &x0, a, 1.0 + 4.0 * a);
+        Ghost::Both.set_border(&mut x);
+        std::mem::swap(&mut x, &mut x0);
+        advect(&mut x, &x0, &uv, dt);
         Ghost::Both.set_border(&mut x);
         std::mem::swap(&mut x, &mut x0);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use glam::Vec2;
 use ndarray::prelude::*;
 use ndarray::Zip;
 
@@ -11,11 +12,11 @@ pub enum Ghost {
 }
 
 pub fn lin_solve(x: &mut Array2<f32>, x0: &Array2<f32>, a: f32, c: f32) {
-    assert_eq!(x.shape(), x0.shape());
+    assert_eq!(x.dim(), x0.dim());
 
     for _ in 0..20 {
-        for i in 1..x.shape()[0] - 1 {
-            for j in 1..x.shape()[1] - 1 {
+        for i in 1..x.dim().0 - 1 {
+            for j in 1..x.dim().1 - 1 {
                 x[[i, j]] = (x0[[i, j]]
                     + a * (x[[i - 1, j]] + x[[i + 1, j]] + x[[i, j - 1]] + x[[i, j + 1]]))
                     / c;
@@ -25,13 +26,13 @@ pub fn lin_solve(x: &mut Array2<f32>, x0: &Array2<f32>, a: f32, c: f32) {
 }
 
 pub fn lin_solve_rayon(x: &mut Array2<f32>, x0: &Array2<f32>, a: f32, c: f32) {
-    assert_eq!(x.shape(), x0.shape());
+    assert_eq!(x.dim(), x0.dim());
 
     let mut t = Array::zeros(x.dim());
     let x1 = &mut t;
 
-    let h = x.shape()[0] - 2;
-    let w = x.shape()[1] - 2;
+    let h = x.dim().0 - 2;
+    let w = x.dim().1 - 2;
 
     for _ in 0..300 {
         Zip::from(x1.slice_mut(s![1..h + 1, 1..w + 1]))
@@ -48,10 +49,45 @@ pub fn lin_solve_rayon(x: &mut Array2<f32>, x0: &Array2<f32>, a: f32, c: f32) {
     }
 }
 
+pub fn advect(d: &mut Array2<f32>, d0: &Array2<f32>, uv: &Array2<Vec2>, dt: f32) {
+    assert_eq!(d.dim(), d0.dim());
+    assert_eq!(uv.dim(), d0.dim());
+
+    let h = d.dim().0 - 2;
+    let w = d.dim().1 - 2;
+
+    let dt0 = dt * ((h * w) as f32).sqrt();
+
+    for i in 1..h + 1 {
+        for j in 1..w + 1 {
+            let y = i as f32 - dt0 * uv[[i, j]].y;
+            let x = j as f32 - dt0 * uv[[i, j]].x;
+
+            let y = y.max(0.5).min(h as f32 + 0.5);
+            let x = x.max(0.5).min(w as f32 + 0.5);
+
+            let i0 = y as usize;
+            let i1 = i0 + 1;
+
+            let j0 = x as usize;
+            let j1 = j0 + 1;
+
+            let t1 = y - i0 as f32;
+            let t0 = 1.0 - t1;
+
+            let s1 = x - j0 as f32;
+            let s0 = 1.0 - s1;
+
+            d[[i, j]] = s0 * (t0 * d0[[i0, j0]] + t1 * d0[[i0, j1]])
+                + s1 * (t0 * d0[[i1, j0]] + t1 * d0[[i1, j1]]);
+        }
+    }
+}
+
 impl Ghost {
     pub fn set_border(self, x: &mut Array2<f32>) {
-        let h = x.shape()[0] - 2;
-        let w = x.shape()[1] - 2;
+        let h = x.dim().0 - 2;
+        let w = x.dim().1 - 2;
 
         for i in 0..h + 2 {
             x[[i, 0]] = if self == Ghost::Vetical {

31d6102c72fb2d8117f96031ad56d01072d9793e
hatoo <hato2000@gmail.com>
2021-09-24 21:29:50 +0900

    wip

--- src/13_flame.rs
+++ src/13_flame.rs
@@ -1,3 +1,4 @@
+use cgmath::vec2;
 use fluid_animations::{
     v2::{advect, gauss_filter, gauss_filter_amb, Mac},
     Float, Ghost,
@@ -10,7 +11,7 @@ fn main() -> anyhow::Result<()> {
 
     let t_amb = 273.0;
 
-    let mut fuel = Array::from_elem((N + 2, N + 2), 0.2);
+    let mut fuel = Array::from_elem((N + 2, N + 2), 0.0);
     let mut t = Array::from_elem((N + 2, N + 2), t_amb);
 
     let mut uv_mac = Mac::zeros((N + 2, N + 2));
@@ -39,6 +40,8 @@ fn main() -> anyhow::Result<()> {
     t[[N / 2, N / 2]] = 10000.0;
     fuel[[N / 2, N / 2]] = 100.0;
 
+    let mut prev_density = Array::from_elem(fuel.dim(), density_amb);
+
     for f in 1..N_FRAME + 1 {
         // fluid_animations::image::save(f, &fuel)?;
         // fluid_animations::image::save(f, &((&t - t_amb) / t_amb))?;
@@ -54,12 +57,18 @@ fn main() -> anyhow::Result<()> {
             }
         });
 
+        /*
         fluid_animations::image::save(
             f,
             &Array::from_shape_fn(fuel.dim(), |(i, j)| {
                 (t[[i, j]] - t_amb) / 750.0 * fuel[[i, j]]
             }),
         )?;
+        */
+
+        fluid_animations::image::save(f, &fuel)?;
+        // fluid_animations::image::save(f, &d_fuel)?;
+        // fluid_animations::image::save(f, &((&t - t_amb) / 2000.0))?;
 
         let density = Array::from_shape_fn(fuel.dim(), |(i, j)| {
             // (density_amb * (1.0 + alpha * s[[i, j]] - beta * (t[[i, j]] - t_amb))).max(0.05)
@@ -67,17 +76,22 @@ fn main() -> anyhow::Result<()> {
             1.01e5 / (285.0 * t[[i, j]]) * (1.0 + alpha * fuel[[i, j]])
         });
 
-        let div = Array::from_shape_fn((N + 2, N + 2), |(i, j)| d_fuel[[i, j]] / dt / 3.0);
+        // let div = Array::from_shape_fn((N + 2, N + 2), |(i, j)| d_fuel[[i, j]] / dt / 3.0);
+        let div = Array::from_shape_fn((N + 2, N + 2), |(i, j)| {
+            -1.0 / dt * (density[[i, j]] - prev_density[[i, j]]) / density[[i, j]]
+        });
 
         /*
         dbg!(density[[N / 2, N / 2]]);
         dbg!(t[[N / 2, N / 2]]);
         dbg!(s[[N / 2, N / 2]]);
         */
+        dbg!(div[[N / 2, N / 2]]);
 
         uv_mac.self_advect(dt / unit);
-        // uv_mac.gauss_filter(uv_sigma2, unit);
         uv_mac.buoyancy2(&density, density_amb, g * dt);
+        uv_mac.gauss_filter(uv_sigma2, unit);
+        // uv_mac.project();
         uv_mac.project_variable_density_div_control(dt, unit, &density, &div);
 
         let uv = uv_mac.create_uv();
@@ -88,11 +102,15 @@ fn main() -> anyhow::Result<()> {
 
         fuel[[N / 2, N / 2]] += dt * N as Float * N as Float * 0.125;
 
+        // uv[[N / 2, N / 2]] = vec2(0.0, -4.0);
+
+        dbg!(uv[[N / 2, N / 2]]);
+
         Zip::from(&mut t)
             .and(&d_fuel)
             .and(&density)
             .for_each(|a, &b, &c| {
-                *a += 600.0 * b / c;
+                *a += 400.0 * b / c;
             });
 
         let mut s1 = gauss_filter(&fuel, fuel_sigma2, unit);
@@ -114,6 +132,10 @@ fn main() -> anyhow::Result<()> {
                 + (1.0 / (*x - t_amb).powi(3) + 3.0 * c * dt / (t_max - *x).powi(4))
                     .powf(-1.0 / 3.0)
         });
+
+        prev_density = density;
+
+        dbg!(t.iter().fold(0.0 as Float, |a, &b| a.max(b)));
 
         eprint!("\rframe: {} / {} done", f, N_FRAME);
     }
